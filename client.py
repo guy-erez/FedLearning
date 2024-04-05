ea40dea69dc5f7480a1954e9252c7b4afd22326f
guy-erez <guy.e12@gmail.com>
2024-04-05 14:16:31 +0300

    each client is a separate run server can work with unlimited amount of clients

--- client.py
+++ client.py
@@ -5,24 +5,17 @@ from torchvision.transforms import ToTensor
 from NnModel import NeuralNetwork,get_device
 import NnModel 
 from dataset import DemoDataset, ToTensor
+import argparse
+import os 
 
-# Set random seed for reproducibility
-torch.manual_seed(27)
-dataset = DemoDataset(csv_file="Anonymous_Demo_Data.csv", label='death', drop = ['reference_date_internal', 'site'],transform=ToTensor())
-training_data, test_data = random_split(dataset, [0.8, 0.2])
-
-client1_size = int(0.00 * len(training_data))
-client2_size = len(training_data) - client1_size
-
-client1_dataset, client2_dataset = random_split(training_data, [client1_size, client2_size])
-datasets_list = [client1_dataset, client2_dataset]
-
-batch_size = 64
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
-
-for i,dataset in enumerate(datasets_list):        
-    # Create data loaders.
-    train_dataloader = DataLoader(dataset, batch_size=batch_size)
+def  main(args):
+    dataset = DemoDataset(csv_file=args.dataset, label='death', drop = ['reference_date_internal', 'site'], transform=ToTensor())
+    training_data, test_data = random_split(dataset, [0.8, 0.2])
+    batch_size = 64
+    test_dataloader = DataLoader(test_data, batch_size=batch_size)
+    i = args.id 
+    # Create data loader
+    train_dataloader = DataLoader(training_data, batch_size=batch_size)
     
     # get avilable device
     device = get_device()
@@ -34,13 +27,25 @@ for i,dataset in enumerate(datasets_list):
     optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
 
     # train loop 
-    epochs = 5
+    epochs = args.epochs
+    os.makedirs('clientModels', exist_ok=True)
     for t in range(epochs):
         print(f"Epoch {t+1}\n-------------------------------")
         NnModel.train(train_dataloader, model, loss_fn, optimizer, device)
         NnModel.test(test_dataloader, model, loss_fn, device)
     print(f"final acc for model {i}")
     NnModel.test(test_dataloader, model, loss_fn, device)
-    model_path = f"model_{i}.pth"
+    model_path = f"clientModels/model_{i}.pth"
     torch.save(model.state_dict(), model_path)
     print(f"Saved PyTorch Model State to {model_path}")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A simple Python script with a main function and a user argument.')
+    parser.add_argument('--id', type=int, help='client id')
+    parser.add_argument('--dataset', type=str, help='path to dataset')
+    parser.add_argument('--epochs', type=int, default=5, help='amount of epochs to train the model')
+
+    args = parser.parse_args()
+    main(args)
+
+
--- server.py
+++ server.py
@@ -6,48 +6,62 @@ from torchvision.transforms import ToTensor
 from NnModel import NeuralNetwork,get_device
 import NnModel
 from dataset import DemoDataset, ToTensor
+import argparse 
+import os
 
-# Set random seed for reproducibility
-torch.manual_seed(27)
-dataset = DemoDataset(csv_file="Anonymous_Demo_Data.csv", label='death', drop = ['reference_date_internal', 'site'],transform=ToTensor())
-training_data, test_data = random_split(dataset, [0.8, 0.2])
-test_dataloader = DataLoader(test_data, batch_size=64)
+def main(args):
+    dataset = DemoDataset(csv_file=args.dataset, label='death', drop = ['reference_date_internal', 'site'],transform=ToTensor())
+    training_data, test_data = random_split(dataset, [0.8, 0.2])
+    test_dataloader = DataLoader(test_data, batch_size=64)
 
+    device = get_device()
+    client_models_pth = os.listdir(args.clientModels)
+    model_list = []
 
-device = get_device()
-model_0 = NeuralNetwork().to(device)
-model_1 = NeuralNetwork().to(device)
-model_list = [model_0,model_1]
-common_model = NeuralNetwork().to(device)
+    for i,client_pth in enumerate(client_models_pth):
+        client_pth_path = os.path.join(args.clientModels,client_pth)
+        model = NeuralNetwork().to(device)
+        model.load_state_dict(torch.load(client_pth_path))
+        model_list.append(model)
+    
+    common_model = NeuralNetwork().to(device)
 
-model_0.load_state_dict(torch.load("model_0.pth"))
-model_1.load_state_dict(torch.load("model_1.pth"))
+    with torch.no_grad():
+        # init common model with zeros 
+        for name, param in common_model.named_parameters():
+            param.data = torch.zeros_like(param)
+        # sum client models    
+        for model in model_list:
+            # print(common_model.linear_relu_stack[0].weight[0])
+            for common_param, param in zip(common_model.parameters(),model.parameters()):
+                common_param.data += param
+        # print(common_model.linear_relu_stack[0].weight[0])
+        # divide each weight  
+        for name, param in common_model.named_parameters():
+            param.data /= len(model_list)
+    
+    # print(common_model.linear_relu_stack[0].weight[0])
 
-with torch.no_grad():
-    for name, param in common_model.named_parameters():
-        param.data = torch.zeros_like(param)
-    for model in model_list:
-        print(common_model.linear_relu_stack[0].weight[0])
-        for common_param, param in zip(common_model.parameters(),model.parameters()):
-            common_param.data += param
-    print(common_model.linear_relu_stack[0].weight[0])
-    for name, param in common_model.named_parameters():
-        param.data /= len(model_list)
-  
-print(common_model.linear_relu_stack[0].weight[0])
+    classes = ['Alive', 'Dead'] 
 
-classes = ['Alive', 'Dead'] 
+    common_model.eval()
+    sample = test_data[0]
+    with torch.no_grad():
+        x = sample['x'].to(device,dtype=torch.float32)
+        pred = common_model(x)
+        predicted, actual = classes[pred.to(int)], classes[sample['y'].to(int)]
+        print(f'Predicted: "{predicted}", Actual: "{actual}"')
+        loss_fn = nn.BCEWithLogitsLoss()
+        print("unified model:")
+        NnModel.test(test_dataloader, common_model, loss_fn, device)
+        for i, model in enumerate(model_list):
+            print(f"model {i}:")
+            NnModel.test(test_dataloader, model, loss_fn, device)
 
-common_model.eval()
-sample = test_data[0]
-with torch.no_grad():
-    x = sample['x'].to(device,dtype=torch.float32)
-    pred = common_model(x)
-    predicted, actual = classes[pred.to(int)], classes[sample['y'].to(int)]
-    print(f'Predicted: "{predicted}", Actual: "{actual}"')
-    loss_fn = nn.BCEWithLogitsLoss()
-    print("unified model:")
-    NnModel.test(test_dataloader, common_model, loss_fn, device)
-    for i, model in enumerate(model_list):
-        print(f"model {i}:")
-        NnModel.test(test_dataloader, model, loss_fn, device)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='A simple Python script with a main function and a user argument.')
+    parser.add_argument('--dataset', type=str, help='path to dataset')
+    parser.add_argument('--clientModels', type=str, help='path to client models dir')
+
+    args = parser.parse_args()
+    main(args)
